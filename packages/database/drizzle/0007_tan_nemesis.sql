a9f233b78eb381e9e552923ba7a46af6c3acd7b1
Khoirul <ahmusafir.khoirul@gmail.com>
2026-06-01 17:05:10 +0700

    feat: add UUID generation for primary keys and update database schema

---
+++ packages/database/drizzle/0007_tan_nemesis.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "categories" ALTER COLUMN "id" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "report_attachments" ALTER COLUMN "id" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "reporters" ALTER COLUMN "id" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "reports" ALTER COLUMN "id" DROP DEFAULT;--> statement-breakpoint
+ALTER TABLE "status_histories" ALTER COLUMN "id" DROP DEFAULT;
